b249b5fb08eea6127e1e02914651dbb7e1f1bd81
sylvester.keinicke <sylvester.keinicke@gmail.com>
2019-01-28 21:21:26 +0100

    search

--- public/edit/sketch.js
+++ public/edit/sketch.js
@@ -9,7 +9,7 @@ function setup() {
     siteInput=document.getElementById("site");
     if(locsearch().site) {
         siteInput.value=locsearch().site;
-        firebase.database().ref().child(locsearch().site).once('value', function(data) {
+        firebase.database().ref().child("sites").child(locsearch().site).once('value', function(data) {
             site=data.val();
             document.body.style.display="inline";
             tarea.style.height=window.innerHeight*0.75+"px";
@@ -24,8 +24,19 @@ function setup() {
 }
 
 function c() {
-    var db=firebase.database().ref().child(siteInput.value);
-    var val={};
-    val.lines=document.getElementById("tarea").value.split("\n");
-    db.set(val);
+    var indexDB=firebase.database().ref().child("index");
+    indexDB.once('value', (data) => {
+        var indexList=data.value;
+        console.log(indexList);
+        indexList[indexList.length]=siteInput.value;
+        indexDB.set(indexList);
+        var db=firebase.database().ref().child("sites").child(siteInput.value);
+        var val={};
+        val.lines=document.getElementById("tarea").value.split("\n");
+        db.set(val);
+    });
+}
+function goback() {
+    var linkSplit=window.location.href.split("edit/");
+    window.location.href=linkSplit.join("site/");
 }
--- public/site/getlocationsearch.js
+++ public/site/getlocationsearch.js
@@ -8,5 +8,4 @@ function locsearch() {
         }
     }
     return val;
-    
 }
